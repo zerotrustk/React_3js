ed34de7844d69d8e89ce1dd59adfd408d7584bc9
kishore kumar <kumar8630@gmail.com>
2025-09-14 23:10:59 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+//<h1>This is a React App</h1>
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-     <h1>This is a React App</h1>
     <App />
   </StrictMode>,
 )
